b41d5c7ef933c88c14315268de11e1fe134511f9
Cavidan Rəcəbli <recebli.cavidan@gmail.com>
2023-06-24 16:51:31 +0400

    Uptade and problem fixed

--- app.js
+++ app.js
@@ -1,31 +1,45 @@
 class NumberGuessingGame {
-    constructor() {
-      this.guesses = 0;
-      this.random_number = Math.floor(Math.random() * 15) + 1;
-    }
-  
-    checkGuess() {
-      let guessInput = document.getElementById("guessInput")
-      let result = document.getElementById("result");
-      const error = document.getElementById("error");
-      let guess = +guessInput.value.trim();
-      if (isNaN(guess)  || guess < 1 || guess > 15) {
-        result.textContent = `Guessed numbers are  ${this.guesses}` ;
-        return;
-      }
-      this.guesses++;
-      if (guess === this.random_number) {
-        result.textContent = "Congratulations! You guessed the correct number in " + this.guesses + " guesses.";
-      } else if (guess < this.random_number) {
-        error.textContent = "Your guess is too low! Try again.";
-      } else {
-        error.textContent = "Your guess is too high! Try again.";
-      }
-  
-      guessInput.value = "";
-      guessInput.focus();
-    }
+  constructor() {
+    this.guesses = 0;
+    this.random_number = this.generateRandomNumber();
+    this.guessInput = document.getElementById("guessInput");
+    this.result = document.getElementById("result");
+    this.error = document.getElementById("error");
   }
-  
-  var game = new NumberGuessingGame();
-  
+
+  generateRandomNumber() {
+    return Math.floor(Math.random() * 15) + 1;
+  }
+
+  checkGuess() {
+    const guess = +this.guessInput.value.trim();
+    if (isNaN(guess) || guess < 1 || guess > 15) {
+      this.result.textContent = `Guessed numbers so far: ${this.guesses}`;
+      return;
+    }
+    this.guesses++;
+    if (guess === this.random_number) {
+      this.displayResult(`Congratulations! You guessed the correct number in ${this.guesses} ${this.guesses === 1 ? 'guess' : 'guesses'}.`);
+    } else if (guess < this.random_number) {
+      this.displayError("Your guess is too low! Try again.");
+    } else {
+      this.displayError("Your guess is too high! Try again.");
+    }
+    this.clearInput();
+  }
+
+  displayResult(message) {
+    this.result.textContent = message;
+  }
+
+  displayError(message) {
+    this.error.textContent = message;
+  }
+
+  clearInput() {
+    this.guessInput.value = "";
+    this.guessInput.focus();
+  }
+}
+
+const game = new NumberGuessingGame();
